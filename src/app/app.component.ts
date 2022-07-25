6b0a1f43f5e11e6114428e496c23c36ecce90f7d
Karl <karlespindola2022@gmail.com>
2022-07-25 12:25:46 -0500

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'ListaCompras';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegistroComponent } from './registro/registro.component';
+import { ItemsComponent } from './items/items.component';
+import { ItemComponent } from './item/item.component';
+import { TotalComponent } from './total/total.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { ServicioListaService } from './servicio-lista.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegistroComponent,
+    ItemsComponent,
+    ItemComponent,
+    TotalComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
-  providers: [],
+  providers: [ServicioListaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
